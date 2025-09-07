0491554c17db98478e5a5784ad1b389c9e519a49
mohamed <om_mahrouk@esi.dz>
2025-09-07 23:50:46 +0100

    fixing paimentRoutes

--- server/routes/paimentRoutes.route.js
+++ server/routes/paimentRoutes.route.js
@@ -1,5 +1,9 @@
 import express from 'express'
+import { protectedRoute } from '../middleware/products.middleware.js'
+import { createCheckoutSession , checkoutSuccess} from '../controllers/paiment.controller.js'
 const router = express.Router()
 
+router.post("/create-checkout-session", protectedRoute, createCheckoutSession);
+router.post("/checkout-success", protectedRoute, checkoutSuccess)
 
 export default router
