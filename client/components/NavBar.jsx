3e89c414341cc382aaea176459d3752458bf61f8
mohamed <om_mahrouk@esi.dz>
2025-09-15 23:44:19 +0100

    changes in the navbar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -20,7 +20,7 @@ const NavBar = () => {
           {/* home */}
           <Link
           to={'/'}
-          className='text-white hover:text-emerald-400 transition duration-300 ease-in-out'>
+          className='text-white hover:text-emerald-400 transition duration-300 ease-in-out text-xl'>
             Home
           </Link>
 
@@ -49,9 +49,9 @@ const NavBar = () => {
 									Sign Up
             </Link>
                      </div>)
-             
-             /* dashbord for admin */
           }
+
+          {/* dashbord for admin */}
           {isAdmin && (
             <Link to={'/secret-dashboard'}
             className='bg-emerald-700 hover:bg-emerald-600 text-white px-3 py-1 rounded-md font-medium
@@ -65,7 +65,7 @@ const NavBar = () => {
           {user && (
             <button
 								className='bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
-						rounded-md flex items-center transition duration-300 ease-in-out'
+						               rounded-md flex items-center transition duration-300 ease-in-out cursor-pointer'
 								onClick={logout}
 							>
 								<LogOut size={18} />
